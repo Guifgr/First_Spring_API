2e370d9df95dfbedbdd037d61233f48df9af8d5f
Guilherme Fernandes <guifgr@hotmail.com>
2021-01-20 23:48:58 -0300

    update

--- learn/src/main/java/com/example/demo/apiExceptionHandler/Error.java
+++ learn/src/main/java/com/example/demo/apiExceptionHandler/Error.java
@@ -1,11 +1,40 @@
 package com.example.demo.apiExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Error {
     private Integer status;
     private LocalDateTime dateHour;
     private String title;
+    private List<Field> Fields;
+
+    public static class Field{
+        private String name;
+        private String message;
+
+        public Field(String name, String message) {
+            setName(name);
+            setMessage(message);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
 
     public Integer getStatus() {
         return status;
@@ -30,4 +59,13 @@ public class Error {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public List<Field> getFields() {
+        return Fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        Fields = fields;
+    }
+
 }
--- learn/src/main/java/com/example/demo/apiExceptionHandler/apiExceptionHandler.java
+++ learn/src/main/java/com/example/demo/apiExceptionHandler/apiExceptionHandler.java
@@ -1,25 +1,41 @@
 package com.example.demo.apiExceptionHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @ControllerAdvice
 public class apiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Autowired
+    private MessageSource messageSource;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var fields = new ArrayList<Error.Field>();
+
+        for(ObjectError error : ex.getBindingResult().getAllErrors()){
+            String name = ((FieldError)error).getField();
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+            fields.add(new Error.Field(name, message));
+        }
 
         var error = new Error();
         error.setStatus(status.value());
         error.setTitle("One or more fields are incorrect or missing! Please review and try again");
         error.setDateHour(LocalDateTime.now());
-
+        error.setFields(fields);
         return super.handleExceptionInternal(ex,error, headers, status, request);
     }
 }
